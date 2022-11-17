3934caaae4b2cdff3cd80d1516f0495d95695733
charizard56 <mrlonzayes@gmail.com>
2022-11-16 22:57:20 -0300

    declaracion de archivos vacioss

--- tp final este/main.cpp
+++ tp final este/main.cpp
@@ -1,18 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 
+
 #include "Funciones.h"
+
 
 using namespace std;
 
 int main() {
 
+    fstream Indata1;
+    fstream Indata2;
+    fstream Indata3;
+    fstream Indata4;
+    fstream Indata5;
+    fstream Indata6;
 
-    Indata1.open("IRI_Consultas.csv");
-    Indata2.open("IRI_Pacientes.csv");
-    Indata3.open("IRI_Contatos.csv");
-    Indata4.open("IRI_Medicos.csv");
-    Indata5.open("IRI_ObraSocial.csv");
 
-    int tam  = 0;
+    Indata1.open(base_path, "IRI_Consultas.csv", ios::out);
+    Indata2.open("IRI_Pacientes.csv", ios :: out);
+    Indata3.open("IRI_Contatos.csv", ios :: out);
+    Indata4.open("IRI_Medicos.csv", ios :: out);
+    Indata5.open("IRI_ObraSocial.csv", ios :: out);
+    Indata6.open("IRI_archivo_pc.csv", ios::out); 
+
+   
+    int tam = 0;
     int tam2 = 0;
     int tam3 = 0;
     int tam4 = 0;
@@ -41,6 +52,8 @@ int main() {
 
     Medico   * array_medicos = new Medico[tam9]; 
 
+    separar_pacientes(array_pacientes, array_consultas, tam, tam5, lista_mas10, tam2, lista_menos10, tam3);
+
 
     delete[] array_pacientes; 
     delete[] lista_mas10;
@@ -53,4 +66,14 @@ int main() {
     getchar();
 
     return 0;
+ 
+
+    Indata1.close(); 
+    Indata2.close();
+    Indata3.close(); 
+    Indata4.close();
+    Indata5.close();
+    Indata6.close();
+
+    return 0; 
 }
